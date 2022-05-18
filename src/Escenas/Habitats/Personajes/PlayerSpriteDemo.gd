cdc9f19a9375f888ce35a791d496c2542fbe48ec
PacMaster <PacMaster@users.noreply.github.com>
2022-05-17 21:11:22 -0500

    Animación movimiento para open world
    controles ya animación para movimiento del jugador en el overview de los escenarios

--- src/Escenas/Habitats/Personajes/PlayerSpriteDemo.gd
+++ src/Escenas/Habitats/Personajes/PlayerSpriteDemo.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 export var walkspeed = 4.0
 const TILESIZE = 16
 
+onready var anim_tree = $AnimationTree
+onready var anim_state = anim_tree.get("parameters/playback")
+
 var initialposition = Vector2(0,0)
 var inputdirection = Vector2(0,0)
 var moving = false
@@ -15,8 +18,10 @@ func _physics_process(delta):
 	if moving == false:
 		process_player_input()
 	elif inputdirection != Vector2.ZERO:
+		anim_state.travel("Walk")
 		move(delta)
 	else:
+		anim_state.travel("Idle")
 		moving = false
 func process_player_input():
 	if inputdirection.y == 0:
@@ -25,8 +30,12 @@ func process_player_input():
 		inputdirection.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	
 	if inputdirection != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", inputdirection)
+		anim_tree.set("parameters/Walk/blend_position", inputdirection)
 		initialposition = position
 		moving = true
+	else:
+		anim_state.travel("Idle")
 		
 func move(delta):
 	percentmovedtonexttile += walkspeed * delta
