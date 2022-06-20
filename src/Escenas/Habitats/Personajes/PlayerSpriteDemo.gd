366f029be10c2056dfeb54ee8b573562a5868608
medio-inestable <medioinestable@gmail.com>
2022-06-20 14:36:24 -0500

    Colliders player 1

--- src/Escenas/Habitats/Personajes/PlayerSpriteDemo.gd
+++ src/Escenas/Habitats/Personajes/PlayerSpriteDemo.gd
@@ -1,11 +1,17 @@
 extends KinematicBody2D
 
 export var walkspeed = 4.0
-const TILESIZE = 32
+const TILESIZE = 16
 
 onready var anim_tree = $AnimationTree
 onready var anim_state = anim_tree.get("parameters/playback")
 onready var ray = $RayCast2D
+
+enum PlayerState { IDLE, TURNING, WALKING}
+enum FacingDirection {LEFT, RIGHT, UP, DOWN}
+
+var player_state = PlayerState.IDLE
+var facing_direction = FacingDirection.DOWN
 
 var initialposition = Vector2(0,0)
 var inputdirection = Vector2(0,0)
@@ -13,10 +19,13 @@ var moving = false
 var percentmovedtonexttile = 0.0
 
 func _ready():
+	anim_tree.active = true
 	initialposition = position
 	
 func _physics_process(delta):
-	if moving == false:
+	if player_state == PlayerState.TURNING:
+		return
+	elif moving == false:
 		process_player_input()
 	elif inputdirection != Vector2.ZERO:
 		anim_state.travel("Walk")
@@ -33,14 +42,40 @@ func process_player_input():
 	if inputdirection != Vector2.ZERO:
 		anim_tree.set("parameters/Idle/blend_position", inputdirection)
 		anim_tree.set("parameters/Walk/blend_position", inputdirection)
-		initialposition = position
-		moving = true
+		anim_tree.set("parameters/Turn/blend_position", inputdirection)
+		
+		if need_to_turn():
+			player_state = PlayerState.TURNING
+			anim_state.travel("Turn")
+		else:
+			initialposition = position
+			moving = true
 	else:
 		anim_state.travel("Idle")
-		
+
+func need_to_turn():
+	var new_facing_direction
+	if inputdirection.x > 0:
+		new_facing_direction = FacingDirection.RIGHT
+	elif  inputdirection.x < 0:
+		new_facing_direction = FacingDirection.LEFT
+	elif  inputdirection.y < 0:
+		new_facing_direction = FacingDirection.UP
+	elif  inputdirection.y > 0:
+		new_facing_direction = FacingDirection.DOWN
+	
+	if facing_direction != new_facing_direction:
+		facing_direction = new_facing_direction
+		return true
+	facing_direction = new_facing_direction
+	return false
+	
+func finished_turning():
+	player_state = PlayerState.IDLE
+	
 func move(delta):
 	percentmovedtonexttile += walkspeed * delta
-	var desired_step: Vector2 = inputdirection * TILESIZE / 2
+	var desired_step: Vector2 = inputdirection * TILESIZE 
 	ray.cast_to = desired_step
 	ray.force_raycast_update()
 	if !ray.is_colliding():
@@ -51,4 +86,5 @@ func move(delta):
 		else:
 			position = initialposition + (TILESIZE * inputdirection * percentmovedtonexttile)
 	else:
+		percentmovedtonexttile = 0.0
 		moving = false
